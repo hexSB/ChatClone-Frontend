9ac5aae4d00c7e386e2ce3688a016418df009bd7
Hextronite <promsunb@gmail.com>
2023-08-12 17:14:18 -0400

    Added protected api request

--- chatClone-project/src/pages/Menu.jsx
+++ chatClone-project/src/pages/Menu.jsx
@@ -6,20 +6,33 @@ import { useState } from 'react';
 const Welcome = () => {
     const { loginWithRedirect, logout, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
     const [chat, setChat] = useState([])
+    const [tokens, setToken] = useState("")
 
     function callApi() {
       axios.get("https://localhost:44306/api/Chat").then(response => {console.log(response.data)})
     }
 
-    async function callProtectedApi() {
+    async function callUserApi() {
       const token = await getAccessTokenSilently();
       const response = await axios.get("https://localhost:44306/api/private", {
         headers:{
-          authorization: "Bearer ${token}"
+          authorization: `Bearer ${token}`
+        }
+      })
+      console.log(response.data,token)
+    }
+
+
+    async function callProtectedApi() {
+      const token = await getAccessTokenSilently();
+      const response = await axios.get("https://localhost:44306/api/private-scoped", {
+        headers:{
+          authorization: `Bearer ${token}`
         }
       })
       console.log(response.data)
     }
+
 
     
     return(
@@ -29,6 +42,9 @@ const Welcome = () => {
               Log Out
             </button>
             <button onClick={callApi}>Get chat</button>
+            <button onClick={callProtectedApi}>Get protected</button>
+            <button onClick={callUserApi}>Get Auth</button>
+
         </div>)
   
 };
