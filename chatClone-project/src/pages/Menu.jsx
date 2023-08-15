9bb72846fc4a795074ea602c80cd7c7d206d1d12
Hextronite <promsunb@gmail.com>
2023-08-14 23:26:38 -0400

    Added Group creation

--- chatClone-project/src/pages/Menu.jsx
+++ chatClone-project/src/pages/Menu.jsx
@@ -5,8 +5,10 @@ import { useState } from 'react';
 
 const Welcome = () => {
     const { loginWithRedirect, logout, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
-    const [chat, setChat] = useState([])
-    const [tokens, setToken] = useState("")
+    const [data, setData] = useState({
+      GroupName: ""
+    })
+
 
     function callApi() {
       axios.get("https://localhost:44306/api/Chat").then(response => {console.log(response.data)})
@@ -34,21 +36,46 @@ const Welcome = () => {
     }
 
     async function callGroup() {
+      const token = await getAccessTokenSilently(); 
+      const response = await axios.post("https://localhost:44306/api/Group", data, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    
+      console.log(response.data);
+    }
+    
+
+    async function GroupGetTest() {
       const token = await getAccessTokenSilently();
       const response = await axios.get("https://localhost:44306/api/Group", {
         headers:{
-          authorization: `Bearer ${token}`
-        },
-        params:{
-          groupName: "Test"
-        },
+          authorization: `Bearer ${token}`,
+          
+        }
       })
       console.log(response.data)
     }
 
 
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setData(prevData => ({
+        ...prevData,
+        [name]: value
+      }));
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log("Submitted data:", data);
+    };
+
+
     
     return(
+      <div>
         <div>
             <h1>Welcome</h1>
             <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
@@ -58,8 +85,25 @@ const Welcome = () => {
             <button onClick={callProtectedApi}>Get protected</button>
             <button onClick={callUserApi}>Get Auth</button>
             <button onClick={callGroup}>Create Group</button>
+            <button onClick={GroupGetTest}>Get Groups</button>
 
-        </div>)
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Group Name:
+              <input
+                type="text"
+                name="GroupName"
+                value={data.GroupName}
+                onChange={handleInputChange}
+              />
+            </label>
+            <button type="submit" onClick={callGroup}>Submit Group</button>
+          </form>
+        </div>
+      </div>
+        )
   
 };
 
